8aac43b2a94c4caa65a767483388d322538319ac
sustainablelab <sustainablelab@gmail.com>
2020-07-02 17:10:15 -0400

    Initial setup.py to run check-manifest

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import setuptools
+
+# Show README.md as PyPI "Project description".
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# utf-8 encoding is for tree symbols: └─, ├─, etc.
+
+setuptools.setup(
+    name="pygs", # no dashes in name!
+    version="0.0.1", # must increment this to re-upload
+    author="Mike Gazes",
+    author_email="sustainablelab@gmail.com",
+    description="PYGame Stuff for writing pygame applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sustainablelab/pygs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+    install_requires=[
+        "pygame",
+        ]
+)
